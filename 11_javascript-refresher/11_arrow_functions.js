a08db5064bab29e9cf0ccfea98e8507afe4abf9b
Ceejay Santos <7078ceejay@gmail.com>
2025-09-04 00:16:01 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,29 @@
+// function greet(name) {
+//   return "Hello, " + name;
+// }
+
+// function square(n) {
+//   return n * n;
+// }
+
+// function sayHi() {
+//   console.log("Hi!");
+// }
+
+const greet = (name) =>{
+    return "Hello, " + name
+}
+
+console.log(greet("Ceejay"))
+
+const square = (n) => {
+    return n * n
+}
+
+console.log(square(2))
+
+const sayHi = () =>{
+    console.log("Hi!")
+}
+
+sayHi()
