e32c87e6013ad5270c071b986fa7ef691aa94c42
Ceejay Santos <7078ceejay@gmail.com>
2025-09-03 18:32:13 +0800

    Add objects activity

---
+++ 11_javascript-refresher/04_objects.js
@@ -0,0 +1,10 @@
+let aboutMe = {
+    name: "Ceejay",
+    age: 12,
+    course: "BSIS",
+
+    introduce: function() {console.log(`Hi, my name is ${this.name} and I am ${this.age} years old.`)}
+
+}
+aboutMe.hobby = "ma2log"
+aboutMe.introduce()
