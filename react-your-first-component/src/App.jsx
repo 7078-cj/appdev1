7f445cf1d8044e7b61d6321d288e626dd8bd8950
7078-cj <7078ceejay@gmail.com>
2025-11-09 21:25:53 +0800

    Done with using a component

--- react-your-first-component/src/App.jsx
+++ react-your-first-component/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Profile from '../components/Profile'
+import Gallery from '../components/Gallery'
 
 function App() {
 
 
   return (
     <>
-      <Profile/>
+      <Gallery/>
     </>
   )
 }
